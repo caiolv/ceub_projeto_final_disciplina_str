f504f0f944b9637290d0621e534a75400c53e622
Caio Oliveira <caiooliveira@MacBook-Pro-de-Caio.local>
2020-10-07 19:51:30 -0300

    fix: variáveis locais e variáveis globais

--- SJF.js
+++ SJF.js
@@ -4,9 +4,9 @@ const reducer = (accumulator, currentValue) => accumulator + currentValue;
 
 function waiting_time(processos) {
     //definindo a qnt. tempo de servico baseado na qnt de processos
-    tempo_servico = Array.from({length: processos.length}, (_, i) => 0);
+    let tempo_servico = Array.from({length: processos.length}, (_, i) => 0);
     //definindo tamanho da waiting list
-    wt = Array.from({length: processos.length}, (_, i) => 0);
+    let wt = Array.from({length: processos.length}, (_, i) => 0);
 
     for (x = 1; x < processos.length; x++) {
       tempo_servico[x] = (parseInt(tempo_servico[x-1]) + parseInt(processos[x-1][2]));
@@ -19,8 +19,8 @@ function waiting_time(processos) {
 
 function turn_around_time(processos) {
     //turnaround time = burstTime + waitingTime
-    tat = Array.from({length: processos.length}, (_, i) => 0);
-    wt = waiting_time(processos)
+    let tat = Array.from({length: processos.length}, (_, i) => 0);
+    let wt = waiting_time(processos)
 
     for (x = 0; x < processos.length; x++)
         tat[x] = processos[x][2] + wt[x];
@@ -28,7 +28,7 @@ function turn_around_time(processos) {
     return tat;
 }
 function average_tat(processos){
-    tat = turn_around_time(processos).reduce(reducer);
+    let tat = turn_around_time(processos).reduce(reducer);
     
     //Retornando o tempo medio 
     //Soma_dos_tat / qnt.Processos
@@ -36,14 +36,13 @@ function average_tat(processos){
 }
 
 function average_wt(processos) {
-    wt = waiting_time(processos).reduce(reducer);
+    let wt = waiting_time(processos).reduce(reducer);
     return (wt / processos.length);
 }
 
 
 function SJF(processos) {
     //Ordenando por Job(Burst time) mais curto
-
     for (var i = 0; i < processos.length; i++) {
         for (var j = 0; j < processos.length - 1; j++) {
             if (processos[j][2] > processos[j+1][2]) {
@@ -53,16 +52,15 @@ function SJF(processos) {
             }
         }
     }
-    
     return processos
 }
 
 //######################################################################
 console.log(":::::::::::::::::::::::::::::::::::SJF:::::::::::::::::::::::::::::::::::")
 
-processos = []
+let processos = []
 
-qtdProcessos = inputInt("Quantidade de processos: ")
+const qtdProcessos = inputInt("Quantidade de processos: ")
 
 for (var i = 0; i < qtdProcessos; i++) {
   let pid = "P"; 
@@ -86,10 +84,10 @@ for (var i = 0; i < qtdProcessos; i++) {
 #                                           #
 #############################################
 */
-wt = waiting_time(processos)
-tat = turn_around_time(processos)
-avg_wt = average_wt(processos)
-avg_tat = average_tat(processos)
+var wt = waiting_time(processos)
+var tat = turn_around_time(processos)
+var avg_wt = average_wt(processos)
+var avg_tat = average_tat(processos)
 console.log("| Process |\t| Burst Time |\t\t| Arrival Time |\t| Waiting Time |\t| Turn-Around Time |\t| Completion Time |\n\n")
 
 for (proc = 0; proc < processos.length; proc++)
