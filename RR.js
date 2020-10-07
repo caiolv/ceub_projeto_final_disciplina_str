99688e4e376a87774d37133125b2d4e8f5d463b7
Caio Oliveira <caiooliveira@MacBook-Pro-de-Caio.local>
2020-10-07 19:48:40 -0300

    fix: numerar os processos

--- RR.js
+++ RR.js
@@ -4,15 +4,15 @@ const reducer = (accumulator, currentValue) => accumulator + currentValue;
 // ROUND-ROBIN
 function round_robin(processos, quantum, qnt_processos) {
     // Criando uma lista de Burst Time restante dos processos
-    bt_restante = Array.from({length: qnt_processos}, (_, i) => 0);
+    let bt_restante = Array.from({length: qnt_processos}, (_, i) => 0);
     // Criando uma lista de Waiting Time
-    wt = Array.from({length: qnt_processos}, (_, i) => 0);
+    let wt = Array.from({length: qnt_processos}, (_, i) => 0);
     // Copiando BurstTime dos processos para o bt_restante
     for (var i = 0; i < qnt_processos; i++)
         bt_restante[i] = processos[i][2]
 
-    tempo = 0 // Tempo total que será adicionado ao WaitingTime
-    overhead = 1 // Valor hipotetico para o tempo gasto na troca de contexto entre processos
+    let tempo = 0 // Tempo total que será adicionado ao WaitingTime
+    let overhead = 1 // Valor hipotetico para o tempo gasto na troca de contexto entre processos
 
     while (true) {
         // Variavel de Controle, verifica se os processos foram
@@ -45,34 +45,34 @@ function round_robin(processos, quantum, qnt_processos) {
         }
         // Se todos os Processos foram concluídos
         if (finalizados)
-            break
+            break;
     }
     return wt // Retornar Lista de WaitingTime
 
 }
 
 function turn_around_time(processos, wt, qnt_processos) { 
-    tat =   Array.from({length: qnt_processos}, (_, i) => 0);
+    let tat = Array.from({length: qnt_processos}, (_, i) => 0);
     for (var i = 0; i < qnt_processos; i++)
         tat[i] = processos[i][2] + wt[i]
     return tat
 }
 
 function average_tat(tat, qnt_processos) { 
-    turnaround_time = tat.reduce(reducer);
+    let turnaround_time = tat.reduce(reducer);
     return (turnaround_time/qnt_processos)
 }
 
 function average_wt(wt, qnt_processos) { 
-    waiting_time = wt.reduce(reducer);
+    let waiting_time = wt.reduce(reducer);
     return (waiting_time/qnt_processos)
 }
 
 //#####################################################################
 
-processos = []
+let processos = []
 
-qtdProcessos = inputInt("Quantidade de processos: ")
+const qtdProcessos = inputInt("Quantidade de processos: ")
 
 for (var i = 0; i < qtdProcessos; i++) {
   let pid = "P"; 
@@ -82,7 +82,7 @@ for (var i = 0; i < qtdProcessos; i++) {
   processos.push([pid, at, bt])
 }
 
-quantum = inputInt("Informe o Quantum: ")
+const quantum = inputInt("Informe o Quantum: ")
 
 /*
 ###########################################
@@ -100,19 +100,19 @@ quantum = inputInt("Informe o Quantum: ")
 */
 
 //Waiting Time
-wt = round_robin(processos, quantum, qtdProcessos)
+const wt = round_robin(processos, quantum, qtdProcessos)
 //TurnAround Time
-tat = turn_around_time(processos, wt, qtdProcessos)
+const tat = turn_around_time(processos, wt, qtdProcessos)
 //Média de todos os TurnAround Time
-avg_tat = average_tat(tat, qtdProcessos)
+const avg_tat = average_tat(tat, qtdProcessos)
 //Média de todos os Waiting Time
-avg_wt = average_wt(wt, qtdProcessos)
+const avg_wt = average_wt(wt, qtdProcessos)
 console.log(`WT = ${wt}\nTAT = ${tat}\nAVG_TAT = ${avg_tat}\nAVG_WT = ${avg_wt}`)
 
 
 console.log("| Process |\t| Burst Time |\t\t| Arrival Time |\t| Waiting Time |\t| Turn-Around Time |\t\n\n")
 for (proc = 0; proc < processos.length; proc++)
-    console.log(`${processos[proc][0]}\t\t\t${processos[proc][2]}\t\t\t${processos[proc][1]}\t\t\t${wt[proc]}\t\t\t         ${tat[proc]}\t\t\t\n`)
+    console.log(`${processos[proc][0] + proc}\t\t\t${processos[proc][2]}\t\t\t${processos[proc][1]}\t\t\t${wt[proc]}\t\t\t         ${tat[proc]}\t\t\t\n`)
 
 console.log(`Average Waiting Time: ${avg_wt}`);
 console.log(`Average Turn-Around Time: ${avg_tat}`);
