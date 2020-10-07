94a4c43c691a5bc3eca9a9a553819de95995b1d5
Caio Oliveira <caiooliveira@MacBook-Pro-de-Caio.local>
2020-10-07 17:39:18 -0300

    add: começar RR

---
+++ RR.js
@@ -0,0 +1,49 @@
+// ROUND-ROBIN
+function round_robin(processos, quantum, qnt_processos) {
+    // Criando uma lista de Burst Time restante dos processos
+    bt_restante =  Array.from({length: qnt_processos}, (_, i) => 0);
+    // Criando uma lista de Waiting Time
+    wt =  Array.from({length: qnt_processos}, (_, i) => 0);
+    // Copiando BurstTime dos processos para o bt_restante
+    for (var i = 0; i < qnt_processos; i++)
+        bt_restante[i] = processos[i][2]
+
+    tempo = 0 // Tempo total que será adicionado ao WaitingTime
+    overhead = 1 // Valor hipotetico para o tempo gasto na troca de contexto entre processos
+
+    while (true) {
+        // Variavel de Controle, verifica se os processos foram
+        // finalizados ou não
+        let finalizados = true;
+        for (var i = 0; i < qnt_processos; i++) {
+            tempo += overhead // Para cada troca de contexto entre os processos, Adicionar ao Tempo Total
+
+            // Se for maior que 0, ainda há processos a serem
+            // Finalizados
+            if (bt_restante[i] > 0) { 
+                finalizados = false;
+                // Se o tempo restante for maior que Quantum 
+                if (bt_restante[i] > quantum) {
+                    // Somar quantum ao tempo de processamento
+                    tempo += quantum
+                    // Retirar do BurstTime restante o Tempo(quantum)
+                    // que ja foi processado
+                    bt_restante[i] -= quantum
+                }
+                else { // Caso o tempo restante seja menor que quantum
+                    // Somar ao tempo, o tempo restante de bt
+                    tempo += bt_restante[i]
+                    // WaitingTime = tempo_total - burst_time do processo
+                    wt[i] = tempo - processos[i][2]
+                    // Zerando burst time
+                    bt_restante[i] = 0
+                }
+            }
+        }
+        // Se todos os Processos foram concluídos
+        if (finalizados)
+            break
+    }
+    return wt // Retornar Lista de WaitingTime
+
+}
